7a245369cce29a40bb4935f7bac60730c97b95b4
Reyes Mauricio <mauricio.reyes@cablasociados.com>
2020-12-01 23:21:28 -0300

    Add project files.

---
+++ XamarinCapacitacion/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinCapacitacion.Views;
+
+namespace XamarinCapacitacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new clase2();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinCapacitacion/Views/clase2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCapacitacion.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class clase2 : ContentPage
+    {
+        public clase2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(entryNumero.Text))
+            {
+                DisplayAlert("Error", "Debe ingresar un Número", "Ok");
+            }
+            else
+            {
+                int number = int.Parse(entryNumero.Text);
+                List<int> resultado = new List<int>();
+
+                for (int i = 1; i <= number; i++)
+                {
+                    resultado.Add(i);
+                }
+
+                ListaResultado.ItemsSource = resultado;
+            }
+        }
+    }
+}
